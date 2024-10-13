fa182b55fc21f0dff4d8b462c355eb09efeb7786
Shubham Patial <techminer27@gmail.com>
2024-10-12 23:51:21 -0400

    Done

--- Dictionary and Set/ProbSol01.py
+++
@@ -1,5 +0,0 @@
-# Question:- Write a program to create a dictionary of Hindi words with values as tehir english translation. Provide the user with an option to look it up!
-
-words = {"Kamra":"Room","Kambal":"Blanket","Fonn":"Phone","Kursi":"Chair","Kapde":"Clothes"}
-meaning = input("Enter any of the following words to know the meaning in english:- 1.) Kamra 2.) Kambal 3.) Fonn 4.) Kursi 5.) Kapde :- ")
-print(f"The meaning of the {meaning} is :-",words[meaning])
