61d6f9fee69443a7452f29d46eaa13ad8db62153
Shubham Patial <shubhampatial27112003@gmail.com>
2024-10-13 23:53:48 -0400

    Done

--- Conditional Statements/if_practice_03.py
+++ Conditional Statements/if_practice_03.py
@@ -4,11 +4,11 @@
 c1 = "Make a lot of money"
 c2 = "buy now"
 c3 = "subscribe this"
-c4 = "click this"
+c4 = "click this" # assigning the value to all the variables
 
-message = input("Type any message you want = ")
+message = input("Type any message you want = ") # taking the user input
 
-if((c1 in message) or (c2 in message) or (c3 in message) or (c4 in message)): # in keyword is used to search the particular word or sentence in the any paragraph or line or sentence
+if((c1 in message) or (c2 in message) or (c3 in message) or (c4 in message)): # checks condition and in keyword is used to search the particular word or sentence in the any paragraph or line or sentence
     print("You can't type this message as this is a spam message")
-else:
+else: # if if statements is false it will run this
     print("Message sent!!")
