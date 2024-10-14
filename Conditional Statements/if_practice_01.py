962dfd5c35162a11546461d111a44a2f071b17d4
Shubham Patial <shubhampatial27112003@gmail.com>
2024-10-13 23:44:20 -0400

    Done

--- Conditional Statements/if_practice_01.py
+++ Conditional Statements/if_practice_01.py
@@ -1,9 +1,9 @@
 a = int(input("Enter the first number = "))
 b = int(input("Enter the second number = "))
 c = int(input("Enter the third number = "))
-d = int(input("Enter the fourth number = "))
-
-if(a > b and a > c and a > d):
+d = int(input("Enter the fourth number = ")) # Taking inputs from the user
+ 
+if(a > b and a > c and a > d): # checks multiple condition and then based of that it produces true or false which leads to the working or non working of that piece of code
     print("First number is the greatest number")
 if(b > a and b > c and b > d):
     print("Second number is the greatest number")
