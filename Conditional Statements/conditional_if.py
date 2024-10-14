fb7b960854b3e76edaec429cceb4d7d87ec8a96a
Shubham Patial <techminer27@gmail.com>
2024-10-13 23:36:54 -0400

    Done

--- Conditional Statements/conditional_if.py
+++ Conditional Statements/conditional_if.py
@@ -2,11 +2,11 @@ age = int(input("Enter your age:- "))
 
 # if elif else ladder
 
-if(age >= 18):
+if(age >= 18): #checks if the age entered by the user is greater than or equal to 18 and if yes, then runs the other two print statements which are within the blocks of if 
     print("You are eligible for driving license")
     print("Happy!!")
 
-elif(age<=0):
+elif(age<=0): #if the if statement was wrong or false then it will check this piece of code and if the condition is true it will run this block of code
     print("Are you ok? Have you ever seen this age")
-else:
-    print("You are not eligible for driving license")
+else: # if none of the if statement is right then this it will run this piece of code
+    print("You are not eligible for driving license") 
