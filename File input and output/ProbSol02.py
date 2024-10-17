ea79351e331d2f4823292437d2ff9981fd644afd
Shubham Patial <shubhampatial27112003@gmail.com>
2024-10-16 23:17:52 -0400

    DONE

--- File input and output/ProbSol02.py
+++ File input and output/ProbSol02.py
@@ -1,19 +1,19 @@
 # Ques:- The game() function in a program lets a user play a game and returns the score as an integer. You need to read a file 'Hi-score.txt' which is either blank or contains the previous Hi - score. You need to write a program to update the Hi - Score whenever game() breaks the Hi-Score
-import random
+import random # python library to import random numbers 
 
-def game():
+def game(): # initializing the function
     print("You are gonna play this game.....")
-    score = random.randint(1, 100) # used to get random number
-    with open("High_Score.txt") as f:
-        high_score = f.read()
-        if high_score != "":
+    score = random.randint(1, 100) # random.randint is used to get random number from and range in this case from 1 to 100
+    with open("High_Score.txt") as f: # opened the file
+        high_score = f.read() # reading the file
+        if high_score != "": # conditional statement  
             high_score = int(high_score)
         else:
             high_score = 0
     print(f"Your socre is = {score}")
-    if score > high_score:
-        with open("High_Score.txt", "w") as f:
-            f.write(str(score))
+    if score > high_score: # Another conditional statement
+        with open("High_Score.txt", "w") as f: # opening the file in write mode
+            f.write(str(score)) # write the score in it
     
     return score
 
