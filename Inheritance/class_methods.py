9a81f4faac189a475a2d46650bdfda766f030a0c
Shubham Patial <shubhampatial27112003@gmail.com>
2024-10-26 22:57:01 -0400

    Done

--- Inheritance/class_methods.py
+++ Inheritance/class_methods.py
@@ -1,11 +1,11 @@
 class Employee:
-    a = 1
+    a = 1 # class attribute
     
-    @classmethod
+    @classmethod # if we comment out this and don't use classmethod then the show method will print 45 as the value because e.a = 45 instance variable so it will print that value but using classmethod will help you print the value of class attribute 
     def show(cls):
         print(f"The class attribute of a is {cls.a}")
 
 e = Employee()
-e.a = 45
+e.a = 45 # instance variable
 
 e.show()
--- Inheritance/operator_overloading.py
+++ Inheritance/operator_overloading.py
@@ -2,8 +2,8 @@ class Number:
     def __init__(self, n):
         self.n = n
 
-    def __add__(self, num):
-        return self.n + num.n
+    def __add__(self, num): # this is overloading which is used to overload operators like + - * / and so on and in this we are adding two object
+        return self.n + num.n # self.n receives the valus of n and num.n receives value of object m
 
 n = Number(1)
 m = Number(2)
