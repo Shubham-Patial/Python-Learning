67e5867ac0eb66cb1bc11e47035d834868ba0694
Shubham Patial <shubhampatial27112003@gmail.com>
2024-10-30 00:37:37 -0400

    Done

--- Advance Python/Problems and Solutions/Prob_Sol_00.py
+++ Advance Python/Problems and Solutions/Prob_Sol_00.py
@@ -1,5 +1,5 @@
 dict_1 = {"Sandwich": 40, "Pizza":60, "Burger":50}
 dict_2 = {"Coffee": 40, "Chai":30, "Water":20}
 
-final_menu = dict_1 | dict_2
+final_menu = dict_1 | dict_2 # merging two dictionaries
 print (final_menu)
